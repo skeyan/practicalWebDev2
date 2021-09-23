4bd675bef206619fd317a4054ce28208714a842d
skeyan <eyan0749@gmail.com>
2021-09-23 19:44:21 -0400

    Implement myMap prototype with basic tests

--- prototypes.js
+++ prototypes.js
@@ -23,13 +23,38 @@ arr = [1,,2,3]
 arr2 = [10,12,14]
 console.log("MY EACH:");
 arr.myEach((element, i) => console.log(element, i));
-console.log("FOR EACH:");
+console.log("FOR EACH (expected):");
 arr.forEach((element, i) => console.log(element, i));
 
 // MAP //
-Array.prototype.myMap = function() {
-
+/**
+ * @param callback function
+ * @return new array
+ * 
+ * map calls a provided callbackFn function once for each element in an array, 
+ * in order, and constructs a new array from the results. callbackFn is invoked only 
+ * for indexes of the array which have assigned values (including undefined).
+ * 
+ * The callback function accepts the following arguments: current element being processed,
+ * (optional) index of curernt element being processed, (optional) array map was called on
+ */
+Array.prototype.myMap = function(callback) {
+    mapped = []
+    for (let i = 0; i < this.length; i++) {
+        if (!(i in this)) {
+            continue;
+        }
+        mapped[i] = callback(this[i], i, this);
+    }
+    return mapped;
 };
+
+/* Tests for MAP */
+arr = [1,undefined,2,3]
+arr2 = [10,12,14]
+arr3 = [1,,2,3]
+console.log("MY MAP:", arr2.myMap(element => element * 2));
+console.log("MAP (expected):", arr2.map(element => element * 2));
 
 // FILTER //
 Array.prototype.myFilter = function() {
